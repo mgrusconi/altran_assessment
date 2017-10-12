876f509b1bee0cf3f3076ca3c050512cf5a12c8c
Marcelo Rusconi <marcelo.rusconi@avantrip.com>
2017-10-12 16:35:44 -0300

    se agregaron los Test en ES6

---
+++ test/app-controller-merge-test.js
@@ -0,0 +1,78 @@
+import 'chai/register-should';
+import request from 'supertest';
+import app from '../server/middlewares/express';
+
+describe('Merge Test', ()=>{
+
+  let messages = {
+    'created': 'New file "file_2_file_1.md" has been created',
+    'not_found': 'not Found',
+    'text': 'Hola yo soy el fichero 2\nA ver si puedo explicar que soy\nHola yo soy el fichero 1'
+  };
+
+  it('Error - merge file1 not found', (done) => {   
+    request(app)
+      .get('/api/merge/error1/file_2')
+      .set({'x-key': '2fvTdG53VCp6z8ZbV66h'})
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .end((err, res) => {
+        should.not.exist(err);
+        res.body.message.should.string(messages.not_found);
+        done();
+      });
+  });
+
+  it('Error - merge file2 not found', (done) => {   
+    request(app)
+      .get('/api/merge/file_1/error2')
+      .set({'x-key': '2fvTdG53VCp6z8ZbV66h'})
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .end((err, res) => {
+        should.not.exist(err);
+        res.body.message.should.string(messages.not_found);
+        done();
+      });
+  });
+
+  it('Successful - Merge - Property', (done) => {   
+    request(app)
+      .get('/api/merge/file_1/file_2')
+      .set({'x-key': '2fvTdG53VCp6z8ZbV66h'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        should.not.exist(err);
+        res.body.should.have.property('message');
+        done();
+      });
+  });
+
+  it('Successful - Merge - Message', (done) => {   
+    request(app)
+      .get('/api/merge/file_1/file_2')
+      .set({'x-key': '2fvTdG53VCp6z8ZbV66h'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        should.not.exist(err);
+        res.body.message.should.equal(messages.created);
+        done();
+      });
+  });
+
+  it('Successful - Merge - Text', (done) => {   
+    request(app)
+      .get('/api/merge/file_1/file_2')
+      .set({'x-key': '2fvTdG53VCp6z8ZbV66h'})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        should.not.exist(err);
+        res.body.text_file.should.equal(messages.text);
+        done();
+      });
+  });
+  
+});
