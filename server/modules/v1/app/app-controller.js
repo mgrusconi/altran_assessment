6a39312cf040929d073cf85f1155775d7328556c
Marcelo Rusconi <marcelo.rusconi@avantrip.com>
2017-10-12 20:56:05 -0300

    Se cambio Callback Hell por readFileSync

--- server/modules/v1/app/app-controller.js
+++ server/modules/v1/app/app-controller.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 /**
  * AppController
  *
@@ -25,63 +24,58 @@ class AppController{
    * @return \Json
    */
   merge(req, res) {
+    
     const file1 = req.params.file1;
     const file2 = req.params.file2;
     const output = file2 + '_' + file1;
-    fs.readFile(config.resources.files + file2 + '.md', {encoding: 'utf8'}, (err, data) => {
+    
+    let file_1_lines = '';
+    let file_2_lines = '';
+        
+    try {
+      file_1_lines = this.readFile(file1);
+      file_2_lines = this.readFile(file2);
+    } catch (err) {
+      return res.status(404).json({
+        'message': 'File ' + file2 + ' not Found',
+        'err': err.toString()
+      });
+    }
+
+    file_1_lines = file_1_lines.split('\n');
+    file_2_lines = file_2_lines.split('\n');
+    let final_file_lines = file_2_lines.slice();
+    
+    for (let i = 0, len = file_1_lines.length; i < len; i++) {
+      if(file_2_lines.indexOf(file_1_lines[i]) == -1){
+        final_file_lines.push(file_1_lines[i]);
+      }
+    }
+
+    let text_file = final_file_lines.join('\n');
+    fs.writeFile(config.resources.files + output + '.md', text_file, { encoding: 'utf-8' }, (err)=>{
       if(err) {
-        if(err.code == 'ENOENT'){
-          return res.status(404).json({
-            'message': 'File ' + file2 + ' not Found',
-            'err': err
-          });
-        }else{
-          return res.status(500).json({
-            'message': 'Internal server error',
-            'err': err
-          });
-        }
+        return res.status(500).json({
+          'message': 'Internal server error',
+          'err': err
+        });
       } else {
-        let file_2_lines = data.split('\n');
-        fs.readFile(config.resources.files + file1 + '.md', {encoding: 'utf8'}, (err, data) => {
-          if(err) {
-            if(err.code == 'ENOENT'){
-              return res.status(404).json({
-                'message': 'File ' + file1 + ' not Found',
-                'err': err
-              });
-            }else{
-              return res.status(500).json({
-                'message': 'Internal server error',
-                'err': err
-              });
-            }
-          } else {
-            let file_1_lines = data.split('\n');
-            let final_file_lines = file_2_lines.slice();
-            for (let i = 0, len = file_1_lines.length; i < len; i++) {
-              if(final_file_lines.indexOf(file_1_lines[i]) == -1){
-                final_file_lines.push(file_1_lines[i]);
-              }
-            }
-            let text_file = final_file_lines.join('\n');
-            fs.writeFile(config.resources.files + output + '.md', text_file, { encoding: 'utf-8' }, (err)=>{
-              if(err) {
-                return res.status(500).json({
-                  'message': 'Internal server error',
-                  'err': err
-                });
-              } else {
-                return res.status(200).json({
-                  'message': 'New file "' + output + '.md" has been created',
-                  'text_file': text_file
-                });
-              }
-            });
-          }
+        return res.status(200).json({
+          'message': 'New file "' + output + '.md" has been created',
+          'text_file': text_file
         });
       }
     });
+  }
+
+  readFile(file){
+    let fileContents = '';
+    try {
+      fileContents = fs.readFileSync(config.resources.files + file + '.md');
+    } catch (err) {
+      throw new Error(err);
+    }
+    return fileContents.toString();
   }
 }
 
